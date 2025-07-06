f082b648fb0e789697b7c0704ca73be14054518e
virginballoon <chaymaezemerki@gmail.com>
2025-07-06 18:50:40 +0100

    6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 let i = 0;
-let c = ["C is fun", "Python is cool", "JavaScript is amazing", null];
-while(c[i] != null )
+let c = ["C is fun", "Python is cool", "JavaScript is amazing"];
+while(c[i])
     {
         console.log(c[i]);
         i++;
